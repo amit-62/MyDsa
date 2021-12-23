d2793fad77a48b9fcaf4c1c7fa5ac37f2de6aea8
amit-62 <kramit6662@gmail.com>
2021-12-23 23:24:44 +0530

    find last occurance

---
+++ last_occurance.cpp
@@ -0,0 +1,33 @@
+//finding last occurance of any number in array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fli(vector<int> a, int idx, int x){
+	if(idx==a.size()){
+		return -1;
+	}
+	int liina = fli(a, idx+1, x);
+	if(liina==-1){
+		if(a[idx]==x){
+			return idx;
+		}
+		else{
+			return -1;
+		}
+	}
+	else return liina;
+}
+
+int main()
+{   
+	int n, x;
+	cin>>n>>x;
+	vector<int> arr(n);
+	for(int i=0; i<n; ++i){
+		cin>>arr[i];
+	}
+
+	cout<<fli(arr, 0, x);
+	return 0;
+}
