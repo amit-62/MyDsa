02a10ce84d5539d78985bc252f7330d4cda12cc4
amit-62 <kramit6662@gmail.com>
2022-03-06 12:25:04 +0530

    Floyd Wrshall Problem

---
+++ basic_FloydWrsl.cpp
@@ -0,0 +1,54 @@
+// application of floyd warshall Algo..
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=500, INF=1e9+10;
+int dist[N][N];
+// vector<pair<int>> g[N];
+
+int main()
+{   
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(i==j){
+				dist[i][j]=0;
+			}
+			else {
+				dist[i][j]=INF;
+			}	
+		}
+	}
+
+	int n,m;
+	cin>>n>>m;
+	for(int i=0; i<m; i++){
+		int x, y, wt;
+		cin>>x>>y>>wt;
+		dist[x][y]=wt;
+		// cout<<x<<y;
+	}
+
+	for(int k=1; k<=n; k++){
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=n; j++){
+				dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+			}
+		}
+	}
+
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			if(dist[i][j]==INF){
+				cout<<"I ";
+			}
+			else {
+				cout<<dist[i][j]<<" ";
+			}
+		}
+		cout<<endl;
+	}
+  
+
+	return 0;
+}
