21859910370f91708d015d973bbc75e404883a57
amit-62 <kramit6662@gmail.com>
2022-03-06 12:55:00 +0530

    DP approach

--- DP_frog1.cpp
+++ DP_frog1.cpp
@@ -5,20 +5,23 @@ using namespace std;
 const int N=1e5+10;
 
 int height[N];
+int dp[N];
 
 int func(int i){
 	if(i==0) return 0;
-
+	if(dp[i]!=-1) return dp[i];
 	int cost=INT_MAX;
+	//brute force approach
 	cost = min(cost, func(i-1)+abs(height[i]-height[i-1]));
 	if(i>1)
 		cost = min(cost, func(i-2)+abs(height[i]-height[i-2]));
 
-	return cost;
+	return dp[i]=cost;
 }
 
 int main()
 {   
+	memset(dp, -1, sizeof(dp));
 	int n;
 	cin>>n;
 	for(int i=0; i<n; i++){
