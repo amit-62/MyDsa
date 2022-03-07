2a8d550c3aa3afb1ca91a7e58321b51aa0cc034b
amit-62 <kramit6662@gmail.com>
2022-03-08 00:13:53 +0530

    Coin change DP Problem

---
+++ LC_coinChange.cpp
@@ -0,0 +1,40 @@
+// A hello world program in C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e4+10;
+int dp[N];
+
+
+int ctCoin(int amount, vector<int>& coins){
+	if(amount==0) return 0;
+	if(dp[amount]!= -1) return dp[amount];
+	int ans= INT_MAX;
+	for(auto coin : coins){
+		if(amount-coin>=0)
+			ans=min(ans +0ll, ctCoin(amount-coin, coins)+1ll);
+	}
+
+	return dp[amount] = ans;
+}
+
+int coinChange(vector<int>& coins, int amount) {
+	memset(dp, -1, sizeof(dp));
+	int ans=ctCoin(amount, coins);
+	return ans==INT_MAX ? -1 : ans;
+        
+}
+
+int main()
+{   
+	int amt, n;
+	cin>>amt>>n;
+	vector<int> coins(n);
+	for(int i=0; i<n; i++){
+		cin>>coins[i];
+	}
+
+	cout<<coinChange(coins,amt);
+	return 0;
+}
---
+++ Lc_coinChange2.cpp
@@ -0,0 +1,44 @@
+// A hello world program in C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e3+10;
+int dp[300][N];
+
+
+int ctCoin(int ind, int amount, vector<int>& coins){
+	if(amount==0) {
+		return 1;
+	}
+	if(ind<0) return 0;
+	if(dp[ind][amount]!= -1) return dp[ind][amount];
+	int ans= 0;
+
+	for(int cnt_amt=0; cnt_amt<=amount; cnt_amt+=coins[ind] ){
+		ans += ctCoin(ind-1, amount-cnt_amt, coins);
+	}
+
+	return dp[ind][amount]=ans;
+	
+}
+
+int coinChange(vector<int>& coins, int amount) {
+	memset(dp, -1, sizeof(dp));
+	int ct=ctCoin(coins.size()-1, amount, coins);
+	return ct;
+        
+}
+
+int main()
+{   
+	int amt, n;
+	cin>>amt>>n;
+	vector<int> coins(n);
+	for(int i=0; i<n; i++){
+		cin>>coins[i];
+	}
+
+	cout<<coinChange(coins, amt);
+	return 0;
+}
